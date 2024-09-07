f1126bea731e43f481c26ba0866af3d8832f07db
Urbine <yohamg@programmer.net>
2024-09-07 17:29:13 +0700

    Commit Summary 6 and 7 Sep 2024: 1. webdriver-test.py - Created functions: extract_title(soup) extract_descriptions(soup), extract_date(soup), extract_source(soup), extract_thumbnail(soup) combine_vid_elems(bs4_obj) get_xml_link(bs4_obj) xml_tag_text(bs4_xml, elem_tag) - Created wp_post_push.py
    Signed-off-by: Urbine <yohamg@programmer.net>

---
+++ src/wp_post_push.py
@@ -0,0 +1,11 @@
+from bs4 import BeautifulSoup
+from main import get_client_info
+import re
+from selenium import webdriver
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import pprint
+import xml
+
+import time
