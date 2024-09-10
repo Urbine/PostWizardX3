c45763e0bfad1b475490903a007d311a248d11b5
Urbine <yohamg@programmer.net>
2024-09-10 23:54:23 +0700

    Commit Summary 10 Sep 2024: 1. Added arguments POST capabilities to wordpress_api.py 2. Created content_select.py 3. Created functions is_parent_dir_required, clean_filename, and search_files_by_ext in main.py 4. General refactoring session
    Signed-off-by: Urbine <yohamg@programmer.net>

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 # This code will be used by several files in this project.
 
 import csv
+import glob
 import json
 import os
-
+import re
 import urllib
 import urllib.request
 import urllib.error
@@ -14,19 +15,87 @@ from requests_oauthlib import OAuth2Session
 # This way OAuthlib won't enforce HTTPS connections.
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
+def clean_filename(filename: str, extension: str=None) -> str:
+    """
+    This function gets a clear filename and handles filenames with and
+    without extension and avoid breaking functions that work with filenames.
+    :param filename: str -> self-explanatory
+    :param extension: str -> self-explanatory
+    :return: str (New filename)
+    """
+    if filename is None:
+        raise RuntimeError(f'You need a filename to continue.')
 
-def get_client_info() -> json:
+    if re.findall("[.+]", extension):
+        pass
+    # This is a kind of "trust" mode.
+    elif extension is None:
+        return filename
+    else:
+        extension = '.' + extension
+
+    if len(filename.split(".")) >= 2:
+        # The user entered a value which included an extension.
+        # and the extension must match what I need.
+        return filename.split(".")[0] + extension
+    else:
+        return filename + extension
+
+
+def is_parent_dir_required(parent: bool) -> str:
+    """
+    This function gets a text that works with other functions to modify the
+    where files are being stored (parent directory or current directory).
+    :param parent: bool that will be gathered by parent functions.
+    :return: str. Empty string in case the parent variable is not provided.
+    """
+    if parent:
+        return '../'
+    elif parent is None:
+        return ''
+    else:
+        return './'
+
+def cwd_or_parent_path(parent: bool) -> str:
+    """
+    This function gets a text that works with other functions to point out
+    where files are being stored (parent directory or current directory).
+    :param parent: bool that will be gathered by parent functions.
+    :return: str
+    """
+    if parent:
+        return os.path.dirname(os.getcwd())
+    else:
+        return os.getcwd()
+
+def get_client_info(filename: str, parent: bool) -> json:
     """
     :return: json object
     """
+    f_name = clean_filename(filename, '.json')
+    in_parent = is_parent_dir_required(parent)
+
     try:
-        with open('../client_info.json', 'r', encoding='utf-8') as secrets:
+        with open(f'{in_parent}{f_name}', 'r', encoding='utf-8') as secrets:
             client_info = json.load(secrets)
         return client_info
     except FileNotFoundError:
         print("File not found! Double-check the filename.")
         print("Don't add the .json extension to the function.")
         return None
+
+def search_files_by_ext(extension: str, parent=False) -> list[str]:
+    """
+    This function returns a list of the .extension files in either parent
+    or current directories.
+    :param extension: with or without dot
+    :param parent: Searches in the parent directory if True, default False.
+    :return: list[str]
+    """
+    # uses the clean_filename function to receive extension with or without dot.
+    search_files = clean_filename('*', extension)
+    return [route.split('/')[-1:][0]
+            for route in glob.glob(is_parent_dir_required(parent)+ f'/{search_files}')]
 
 
 def get_token_oauth(client_id_, uri_callback_, client_secret_, auth_url_, token_url_) -> json:
@@ -79,17 +148,9 @@ def import_request_json(filename, parent=False) -> list:
     :param filename: (str) filename
     :return: json object
     """
-    if parent:
-        parent_or_cwd = '../'
-    else:
-        parent_or_cwd = './'
+    parent_or_cwd = is_parent_dir_required(parent)
+    json_file = clean_filename(filename, '.json')
 
-    if len(filename.split(".")) >= 2:
-        json_file = filename
-    elif filename == '':
-        raise RuntimeError("You need a JSON file to continue.")
-    else:
-        json_file = filename + '.json'
     try:
         with open(f"{parent_or_cwd}{json_file}", 'r', encoding='utf-8') as f:
             imp_json = json.load(f)
@@ -99,22 +160,19 @@ def import_request_json(filename, parent=False) -> list:
         return None
 
 
-def export_request_json(filename, stream, indent, parent=False) -> None:
+def export_request_json(filename, stream, indent=1, parent=False) -> None:
     """
     :param filename: (str) Filename without JSON extension.
     :param stream: (json) Data stream to export to JSON
-    :param indent: (int) Indentation spaces
+    :param indent: (int) Indentation spaces. Default 1
     :param parent: (bool) Place file in parent directory if True, default False.
     :return: (None) print statement for console logging.
     """
-    if parent:
-        filename = "../" + filename
-        f_path = os.path.dirname(os.getcwd())
-    else:
-        filename = "./" + filename
-        f_path = os.getcwd()
+    f_name = clean_filename(filename,'.json')
+    filename = is_parent_dir_required(parent) + f_name
+    f_path = cwd_or_parent_path(parent)
 
-    with open(f'{filename}.json', 'w', encoding='utf-8') as t:
+    with open(f'{filename}', 'w', encoding='utf-8') as t:
         json.dump(stream, t, ensure_ascii=False, indent=indent)
     return print(f"The JSON file has been stored in {f_path}. Ready to use!\n")
 
@@ -137,11 +195,15 @@ def export_to_csv_nt(nmedtpl_lst, filename, top_row_lst) -> None:
                 continue
     return None
 
-
 if __name__ == '__main__':
     access_url()
     access_url_bs4()
+    clean_filename()
+    cwd_or_parent_path()
+    export_request_json()
     get_client_info()
     get_token_oauth()
     import_request_json()
-    export_request_json()
+    is_parent_dir_required()
+    search_files_by_ext()
+
