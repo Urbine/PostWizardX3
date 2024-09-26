6d134ebca2fcad11acc91c6cac9a277120d291c7
Urbine <yohamg@programmer.net>
2024-09-26 16:25:19 +0700

    feat(helpers): add file, database, and date utilities
    - Add filename_select (replaces database_select), db_creation_helper, if_exists_remove, load_from_file, write_to_file, parse_date_to_iso.
    - Default parent=False on get_client_info/cwd_or_parent_path; normalize glob path segment; relocate access_url/is_parent_dir_required and JSON export-import helpers.

--- src/helpers.py
+++ src/helpers.py
@@ -9,11 +9,36 @@ import urllib
 import urllib.request
 import urllib.error
 
+from datetime import date
+
 from bs4 import BeautifulSoup
+from calendar import month_abbr, month_name
 from requests_oauthlib import OAuth2Session
 
 # This way OAuthlib won't enforce HTTPS connections.
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+
+def access_url_bs4(url_to_bs4: str) -> BeautifulSoup:
+    """
+    :param url_to_bs4: (str) URL
+    :return: bs4.BeautifulSoup object
+    """
+    user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
+    headers = {'User-Agent': user_agent, }
+    request = urllib.request.Request(url_to_bs4, None, headers)
+    page = urllib.request.urlopen(request)
+    return BeautifulSoup(page, 'html.parser')
+
+def access_url(url_raw: str):
+    """
+    :param url_raw: (str) URL
+    :return: http.client.HTTPResponse object
+    """
+    user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
+    headers = {'User-Agent': user_agent, }
+    request = urllib.request.Request(url_raw, None, headers)
+    page = urllib.request.urlopen(request)
+    return page
 
 def clean_filename(filename: str, extension: str=None) -> str:
     """
@@ -41,22 +66,7 @@ def clean_filename(filename: str, extension: str=None) -> str:
     else:
         return filename + extension
 
-
-def is_parent_dir_required(parent: bool) -> str:
-    """
-    This function gets a text that works with other functions to modify the
-    where files are being stored (parent directory or current directory).
-    :param parent: bool that will be gathered by parent functions.
-    :return: str. Empty string in case the parent variable is not provided.
-    """
-    if parent:
-        return '../'
-    elif parent is None:
-        return ''
-    else:
-        return './'
-
-def cwd_or_parent_path(parent: bool) -> str:
+def cwd_or_parent_path(parent: bool=False) -> str:
     """
     This function gets a text that works with other functions to point out
     where files are being stored (parent directory or current directory).
@@ -68,38 +78,91 @@ def cwd_or_parent_path(parent: bool) -> str:
     else:
         return os.getcwd()
 
-def get_client_info(filename: str, parent: bool) -> json:
+def db_creation_helper(db_suggestions: list[str]) -> str:
     """
-    :return: json object
+    Takes a list of suggested names or creates a custom .db file.
+    :param db_suggestions: list with the suggested db names
+    :return: db name either suggested or customised
     """
-    f_name = clean_filename(filename, '.json')
-    in_parent = is_parent_dir_required(parent)
+    db_name_suggest = db_suggestions
+    print('Suggested database names:\n')
+    for num, db in enumerate(db_name_suggest, start=1):
+        print(f'{num}. {db}')
 
+    db_name_select = input('\nPick a number to create your database or else type in a name now: ')
     try:
-        with open(f'{in_parent}{f_name}', 'r', encoding='utf-8') as secrets:
-            client_info = json.load(secrets)
-        return client_info
-    except FileNotFoundError:
-        print("File not found! Double-check the filename.")
-        print("Don't add the .json extension to the function.")
-        return None
+        return db_name_suggest[int(db_name_select) - 1]
+    except ValueError or IndexError:
+        if len(db_name_select.split(".")) >= 2:
+            return db_name_select
+        elif db_name_select == '':
+            raise RuntimeError("You really need a database name to continue.")
+        else:
+            return db_name_select + '.db'
 
-def search_files_by_ext(extension: str, parent=False, folder='') -> list[str]:
+def filename_select(extension: str, parent: bool=False) -> str:
     """
-    This function returns a list of the .extension files in either parent
-    or current directories.
-    :param extension: with or without dot
-    :param parent: Searches in the parent directory if True, default False.
-    :param folder: str folder name followed by '/'
-    :return: list[str]
+    Gives you a list of files with a certain extension.
+    :param extension: File extension of the files that interest you.
+    :param parent: Bool, True to search in parent dir, default set to False.
+    :return: File name without relative path.
+    If you want to access the file from a parent dir,
+    either let the destination function handle it for you or specify it yourself.
     """
-    # uses the clean_filename function to receive extension with or without dot.
-    search_files = clean_filename('*', extension)
-    return [route.split('/')[-1:][0]
-            for route in glob.glob(is_parent_dir_required(parent)+ f'/{folder}{search_files}')]
+    available_files = search_files_by_ext(extension, parent=parent)
+    print(f'\nHere are the available {extension} files:')
+    for num, file in enumerate(available_files, start=1):
+        print(f'{num}. {file}')
+
+    select_file = input(f'\nSelect your {extension} file now: ')
+    try:
+        return available_files[int(select_file) - 1]
+    except IndexError:
+        raise RuntimeError(f'This program requires a {extension} file. Exiting...')
+
+def export_request_json(filename: str,
+                        stream,
+                        indent: int = 1,
+                        parent: bool=False) -> None:
+    """
+    :param filename: (str) Filename without JSON extension.
+    :param stream: (json) Data stream to export to JSON
+    :param indent: (int) Indentation spaces. Default 1
+    :param parent: (bool) Place file in parent directory if True, default False.
+    :return: (None) print statement for console logging.
+    """
+    f_name = clean_filename(filename,'.json')
+    filename = is_parent_dir_required(parent) + f_name
+    f_path = cwd_or_parent_path(parent)
+
+    with open(f'{filename}', 'w', encoding='utf-8') as t:
+        json.dump(stream, t, ensure_ascii=False, indent=indent)
+    return print(f"The JSON file has been stored in {f_path}. Ready to use!\n")
 
 
-def get_token_oauth(client_id_, uri_callback_, client_secret_, auth_url_, token_url_) -> json:
+def export_to_csv_nt(nmedtpl_lst: list, filename: str, top_row_lst: list[str]) -> None:
+    """
+    Helper function to dump the list of NamedTuples into a CSV file in project dir.
+    :param nmedtpl_lst: list[namedtuple]
+    :param filename: name (without csv extension)
+    :param top_row_lst: list[str]
+    :return: None (file in project root)
+    """
+    with open(filename + '.csv', 'w', newline='', encoding='utf-8') as csvfile:
+        writer = csv.writer(csvfile, dialect='excel', )
+        writer.writerow(top_row_lst)
+        for tag in nmedtpl_lst:
+            try:
+                writer.writerow([getattr(tag, f) for f in tag._fields])
+            except IndexError:
+                continue
+    return None
+
+def get_token_oauth(client_id_: str,
+                    uri_callback_: str,
+                    client_secret_: str,
+                    auth_url_: str,
+                    token_url_: str) -> json:
     """
     :param client_id_: (str)
     :param uri_callback_: (str)
@@ -118,32 +181,47 @@ def get_token_oauth(client_id_, uri_callback_, client_secret_, auth_url_, token_
         client_secret=client_secret_)
     return token
 
-
-def access_url_bs4(url_to_bs4) -> BeautifulSoup:
+def if_exists_remove(fname: str):
     """
-    :param url_to_bs4: (str) URL
-    :return: bs4.BeautifulSoup object
+    Removes a file if it exists.
+    :param fname: File name
+    :return: Returns none if the file does not exist
     """
-    user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
-    headers = {'User-Agent': user_agent, }
-    request = urllib.request.Request(url_to_bs4, None, headers)
-    page = urllib.request.urlopen(request)
-    return BeautifulSoup(page, 'html.parser')
+    if os.path.exists(fname):
+        os.remove(fname)
+    else:
+        return None
 
-
-def access_url(url_raw):
+def is_parent_dir_required(parent: bool) -> str:
     """
-    :param url_raw: (str) URL
-    :return: http.client.HTTPResponse object
+    This function gets a text that works with other functions to modify the
+    where files are being stored (parent directory or current directory).
+    :param parent: bool that will be gathered by parent functions.
+    :return: str. Empty string in case the parent variable is not provided.
     """
-    user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
-    headers = {'User-Agent': user_agent, }
-    request = urllib.request.Request(url_raw, None, headers)
-    page = urllib.request.urlopen(request)
-    return page
+    if parent:
+        return '../'
+    elif parent is None:
+        return ''
+    else:
+        return './'
 
+def get_client_info(filename: str, parent: bool=False):
+    """
+    :return: json object
+    """
+    f_name = clean_filename(filename, '.json')
+    in_parent = is_parent_dir_required(parent)
 
-def import_request_json(filename, parent=False) -> list:
+    try:
+        with open(f'{in_parent}{f_name}', 'r', encoding='utf-8') as secrets:
+            client_info = json.load(secrets)
+        return client_info
+    except FileNotFoundError:
+        print("File not found! Double-check the filename.")
+        return None
+
+def import_request_json(filename: str, parent: bool=False) -> list:
     """
     :param parent: Looks for the JSON file in the parent directory if True, default False.
     :param filename: (str) filename
@@ -160,66 +238,70 @@ def import_request_json(filename, parent=False) -> list:
         print("File not found! Double-check the filename.")
         return None
 
-
-def export_request_json(filename, stream, indent=1, parent=False) -> None:
+def load_from_file(filename: str, extension: str, parent=False) -> str:
     """
-    :param filename: (str) Filename without JSON extension.
-    :param stream: (json) Data stream to export to JSON
-    :param indent: (int) Indentation spaces. Default 1
-    :param parent: (bool) Place file in parent directory if True, default False.
-    :return: (None) print statement for console logging.
+    :param extension: file extension of the file to be read
+    :param parent: Looks for the file in the parent directory if True, default False.
+    :param filename: (str) filename
+    :return: str
     """
-    f_name = clean_filename(filename,'.json')
-    filename = is_parent_dir_required(parent) + f_name
-    f_path = cwd_or_parent_path(parent)
+    parent_or_cwd = is_parent_dir_required(parent)
+    file = clean_filename(filename, extension)
 
-    with open(f'{filename}', 'w', encoding='utf-8') as t:
-        json.dump(stream, t, ensure_ascii=False, indent=indent)
-    return print(f"The JSON file has been stored in {f_path}. Ready to use!\n")
-
-
-def export_to_csv_nt(nmedtpl_lst, filename, top_row_lst) -> None:
-    """
-    Helper function to dump the list of NamedTuples into a CSV file in project dir.
-    :param nmedtpl_lst: list[namedtuple]
-    :param filename: name (without csv extension)
-    :param top_row_lst: list[str]
-    :return: None (file in project root)
-    """
-    with open(filename + '.csv', 'w', newline='', encoding='utf-8') as csvfile:
-        writer = csv.writer(csvfile, dialect='excel', )
-        writer.writerow(top_row_lst)
-        for tag in nmedtpl_lst:
-            try:
-                writer.writerow([getattr(tag, f) for f in tag._fields])
-            except IndexError:
-                continue
-    return None
-
-
-def database_select(parent=False) -> str:
-    available_dbs = search_files_by_ext('db', parent=parent)
-    print('\nHere are the available .db files:')
-    for num, db in enumerate(available_dbs, start=1):
-        print(f'{num}. {db}')
-
-    select_db = input('\nSelect your database now: ')
     try:
-        return is_parent_dir_required(parent=parent) + available_dbs[int(select_db) - 1]
-    except IndexError:
-        raise RuntimeError('This program requires a database. Exiting...')
+        with open(f"{parent_or_cwd}{file}", 'r', encoding='utf-8') as f:
+            return f.read()
+    except FileNotFoundError:
+        print("File not found! Double-check the filename.")
+        return None
 
-# Removing this as it is not necessary.
-# if __name__ == '__main__':
-#     access_url()
-#     access_url_bs4()
-#     clean_filename()
-#     cwd_or_parent_path()
-#     database_select()
-#     export_request_json()
-#     get_client_info()
-#     get_token_oauth()
-#     import_request_json()
-#     is_parent_dir_required()
-#     search_files_by_ext()
+def parse_date_to_iso(full_date: str, m_abbr: bool=False) -> date:
+    """
+    Break down the date and convert it to ISO format to get a datetime.date object.
+    important: 'from calendar import month_abbr, month_name' is required.
+    :param full_date: date_full is 'Aug 20th, 2024' or 'August 20th, 2024'.
+    :param m_abbr: Set to True if you provide a month abbreviation, default set to False ().
+    :return: date object (ISO format)
+    """
+    # Lists month_abbr and month_name provided by the Calendar library in Python.
+    if m_abbr:
+        months = [m for m in month_abbr]
+    else:
+        months = [m for m in month_name]
+
+    year = str(full_date.split(",")[1].strip())
+    month_num = str(months.index(full_date.split(",")[0].split(" ")[0]))
+
+    # The date ISO format requires that single numbers are preceded by a 0.
+    if int(month_num) <= 9:
+        month_num = '0' + str(months.index(full_date.split(",")[0].split(" ")[0]))
+
+    day_nth = str(full_date.split(",")[0].split(" ")[1])
+    day = day_nth.strip("".join(re.findall("[a-z]", day_nth)))
+
+    if int(day) <= 9:
+        day_date = '0' + day_nth.strip("".join(re.findall("[a-z]", day_nth)))
+
+    return date.fromisoformat(year + month_num + day)
+
+def search_files_by_ext(extension: str, parent: bool = False, folder:str = '') -> list[str]:
+    """
+    This function returns a list of the .extension files in either parent
+    or current directories.
+    :param extension: with or without dot
+    :param parent: Searches in the parent directory if True, default False.
+    :param folder: str folder name followed by '/'
+    :return: list[str]
+    """
+    # uses the clean_filename function to receive extension with or without dot.
+    search_files = clean_filename('*', extension)
+    return [route.split('/')[-1:][0]
+            for route in glob.glob(is_parent_dir_required(parent)+ f'/{folder}/{search_files}')]
+
+def write_to_file(filename: str, extension: str, stream, parent: bool=False) -> None:
+    f_name = clean_filename(filename, extension)
+    with open(f'{is_parent_dir_required(parent=parent)}{f_name}', 'w', encoding='utf-8') as file:
+        file.write(str(stream))
+    print(f'Created file {f_name} in {cwd_or_parent_path(parent=parent)}')
+    return None
 
