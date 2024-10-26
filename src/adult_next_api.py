8abe8a065546e3661fe39b016b821cd3e03b0c50
Urbine <yohamg@programmer.net>
2024-10-26 19:18:01 +0700

    Commit Summary - 26 Oct 2024 feat: create `adult_next_api.py` with API dump URL constructor and database parser - Created new file `adult_next_api.py` to handle API dump URL construction and data parsing. - Added `construct_api_dump_url` function for customizable API URL generation, supporting parameters like campaign ID, sorting, and separators. - Implemented `adult_next_dump_parse` function to parse API dump data and store it in a local SQLite database, defining an `embeds` table for metadata (ID, title, description, link, etc.). - Included custom slug creation based on partner and content type for structured URL generation. - Updated `helpers.py` for readability in `match_list` function with additional line spacing. - Minor formatting fix in `wordpress_api.py` (removed newline at EOF).
    Signed-off-by: Urbine <yohamg@programmer.net>

---
+++ src/adult_next_api.py
@@ -0,0 +1,179 @@
+import datetime
+import sqlite3
+
+# Local implementations
+import helpers
+
+# Constants
+
+ABJAV_BASE_URL = 'https://direct.abjav.com'
+ABJAV_CAMPAIGN_ID = 1291575419
+
+# Encoded separators for the URL params
+PIPE_SEP = '%7C'
+COMMA_SEP = '%2C'
+SEMICOLON_SEP = '%3B'
+
+
+# Functions
+
+def construct_api_dump_url(base_url: str,
+                           campgn_id: str | int,
+                           sort_crit: str,
+                           sep: str,
+                           days: str | int = '',
+                           url_limit: str | int = 999999999) -> str:
+    params = '/feeds/?link_args='
+    campaign_id = f'campaign_id:{campgn_id}'
+    format = "&feed_format=csv&"
+    sorting = f'sorting={sort_crit}&'  # rating, popularity, duration, post_date, ID
+    limit = f'limit={url_limit}'
+    sep_param = f'&csv_separator={sep}&'
+    if days != '': days = f'days={days}&'
+
+    # Column Fields
+    _ID = 'id'
+    _title = 'title'
+    _description = 'description'
+    _link = 'link'
+    _duration = 'duration'  # seconds
+    _rating = 'rating'
+    _added_time = 'post_date'
+    _categories = 'categories'
+    _tags = 'tags'
+    _model = 'models'
+    _embed_code = 'embed'
+    _thumbnail_prefix = 'screenshots_prefix'  # Attaches to main to fetch thumbnail.
+    _main_thumbnail = 'main_screenshot'  # Thumbnail ID obtained from removing the img extension
+    _thumbnails = 'screenshots'
+    _video_thumbnail_url = 'preview_url'
+
+    column_lst = [
+        _ID,
+        _title,
+        _description,
+        _link,
+        _duration,
+        _rating,
+        _added_time,
+        _categories,
+        _tags,
+        _model,
+        _embed_code,
+        _thumbnail_prefix,
+        _main_thumbnail,
+        _thumbnails,
+        _video_thumbnail_url
+    ]
+
+    csv_columns = f'csv_columns={sep.join(column_lst)}'
+
+    return f"{base_url}{params}{campaign_id}{format}{sorting}{days}{limit}{sep_param}{csv_columns}"
+
+
+def adult_next_dump_parse(filename: str,
+                          dirname: str,
+                          partner: str,
+                          sep: str,
+                          parent: bool = False):
+    # ID|Title|Description|Website link|Duration|Rating|Publish date, time|Categories|Tags|Models|Embed code|Thumbnail prefix|Main thumbnail|Thumbnails|Preview URL
+    if dirname:
+        path = f"{dirname}/{helpers.clean_filename(filename, 'csv')}"
+    else:
+        path = f"{helpers.is_parent_dir_required(parent=parent)}{helpers.clean_filename(filename, 'txt')}"
+
+    db_name = f'{helpers.is_parent_dir_required(parent=parent)}{filename}-{datetime.date.today()}.db'
+    db_conn = sqlite3.connect(db_name)
+    db_cur = db_conn.cursor()
+
+    db_create_table = """
+    CREATE TABLE embeds(id, 
+                        title, 
+                        description, 
+                        web_link, 
+                        duration, 
+                        rating, 
+                        date, 
+                        categories, 
+                        tags, 
+                        models, 
+                        embed_code, 
+                        thumbnail_prefix, 
+                        thumbnail_name, 
+                        video_thumbnails, 
+                        video_trailer, 
+                        wp_slug)
+    """
+    db_cur.execute(db_create_table)
+
+    total_entries = 0
+    with open(path, 'r', encoding='utf-8') as dump_file:
+        for line in dump_file.readlines():
+            # I don't want to process or count the header of the csv file.
+            if total_entries == 0:
+                total_entries += 1
+                continue
+            else:
+                line_split = line.split(sep)
+                id_ = line_split[0]
+                title = line_split[1]
+                description = line_split[2]
+                website_link = line_split[3]
+                duration = line_split[4]
+                rating = line_split[5]
+                publish_date = line_split[6].split(' ')[0]
+                categories = line_split[7]
+                tags = line_split[8]
+                models = line_split[9]
+                embed_code = line_split[10]
+                thumbnail_prefix = line_split[11]
+                main_thumbnail = line_split[12]
+                thumbnails = line_split[13]
+                video_trailer = line_split[14].strip('\n')
+
+                # Custom db fields
+
+                # As mentioned in other modules, slugs have to contain the content type
+                wp_slug = f"{partner}-{website_link.split('/')[-2:][0]}-video"
+
+                all_values = (id_,
+                              title,
+                              description,
+                              website_link,
+                              duration,
+                              rating,
+                              publish_date,
+                              categories,
+                              tags,
+                              models,
+                              embed_code,
+                              thumbnail_prefix,
+                              main_thumbnail,
+                              thumbnails,
+                              video_trailer,
+                              wp_slug)
+
+                db_cur.execute("INSERT INTO embeds values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", all_values)
+                db_conn.commit()
+                total_entries += 1
+
+    db_conn.close()
+    return print(total_entries)
+
+
+if __name__ == '__main__':
+    main_url = construct_api_dump_url(ABJAV_BASE_URL,
+                                      ABJAV_CAMPAIGN_ID,
+                                      'popularity',
+                                      PIPE_SEP, days=30)
+
+    helpers.write_to_file('abjav-dump',
+                          'tmp',
+                          'csv',
+                          helpers.access_url_bs4(main_url),
+                          parent=True)
+
+    partners = ['abjav']
+
+    adult_next_dump_parse('abjav-dump', '../tmp',
+                          partners[0], '|', parent=True)
