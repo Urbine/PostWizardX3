0e052a933267072de04bd6212427369bf6151daa
Urbine <yohamg@programmer.net>
2024-11-12 21:37:04 +0700

    feat(integrations): add url_builder CSV primitives and feed_beta API
    New `url_builder.py`: frozen `CSVColumns` (16 column fields) and `CSVSeparators` (pipe/comma/semicolon) dataclasses.
    New `feed_beta_api.py`: `construct_feed_beta_dump_url`, `feed_beta_dump_parse` (10-column embeds SQLite schema), argparse CLI main (dump -> parse -> tmp cleanup).

---
+++ integrations/feed_beta_api.py
@@ -0,0 +1,196 @@
+"""
+Gather information from the feed_beta API, parse it and insert it into a SQLite3 database.
+"""
+
+# Std Library
+import argparse
+import datetime
+import os
+import sqlite3
+
+# Local implementations
+import common
+from .url_builder import CSVColumns, CSVSeparators
+from workflows import clean_file_cache
+
+PartnerOne_BASE_URL = 'https://example-feed-a.com/admin/feeds/embed/?source=576422190'
+
+
+def construct_feed_beta_dump_url(
+        base_url: str,
+        sort_crit: str,
+        days: str | int = "",
+        url_limit: str | int = 999999999,
+        sep: CSVSeparators = CSVSeparators.pipe_sep,
+        columns: CSVColumns = CSVColumns
+) -> str:
+    """
+    Construct the API dump url to access the text content to be parsed by function `feed_alpha_dump_parse`
+    once accessed.
+
+    :param base_url: API Base URL (provided in this module as a constant)
+    :param sort_crit: Sorting criteria from possible values:
+
+                      1. ``'popularity'``
+                      2. ``'rating'``
+                      3. ``'duration'``
+                      4. ``'post_date'``
+                      5. ``'id'`` **(default if no criteria is provided)**
+
+    :param sep: ``CSVSeparator`` Object that contains the dump separation code
+    :param days: time period scope in days
+    :param url_limit: maximum value of URLs to retrieve.
+    :param columns: ``CSVColumns`` object that contains common csv columns.
+    :return: ``f-str`` (Formatted str) Video Dump URL
+    """
+    format = "&feed_format=csv&"
+    screenshot_format = 'screenshot_format=source&'
+    # rating, popularity, duration, post_date, ID
+    sorting = f"sorting={sort_crit}&"
+    limit = f"limit={url_limit}"
+    sep_param = f"&csv_separator={sep}&"
+    days = f"days={days}&" if days != "" else days
+
+    # Column Fields (These can be extended or removed)
+    column_lst = [
+        columns.ID_,
+        columns.title,
+        columns.duration,
+        columns.added_time,
+        columns.categories,
+        columns.rating,
+        columns.main_thumbnail,
+        columns.embed_code,
+        columns.link
+    ]
+
+    csv_columns = f"csv_columns={str(sep).join(column_lst)}"
+
+    return f"{base_url}{format}{screenshot_format}{sorting}{days}{limit}{sep_param}{csv_columns}"
+
+
+def feed_beta_dump_parse(filename: str, dirname: str,
+                    partner: str, sep: str) -> str:
+    """
+    Parse the text dump based on the parameters that ``construct_feed_beta_dump_url`` constructed.
+    Once the temporary text file is fetched from the origin, this function will record all the values
+    into a ``SQLite3`` database.
+
+    :param filename: ``str`` temporary ``CSV`` filename that will be used as the database filename
+    :param dirname: ``str`` provide a target path for the database
+    :param partner: ``str`` Partner offer name that will be used to construct the video slugs in the database.
+    :param sep: ``str`` character separator depending on the separator constant selected at URL build (typically ``'|'``).
+    :return: ``f-str`` (Formatted str) with the video count and database name.
+    """
+    # Sample layout of the text file to be parsed, however, not the actual dump.
+    # ID|Title|Description|Website link|Duration|Rating|Publish date,
+    # time|Categories|Tags|Models|Embed code|Thumbnail prefix|Main
+    # thumbnail|Thumbnails|Preview URL
+    c_filename = common.clean_filename(filename, 'csv')
+    is_parent_dir = False if os.path.exists(
+        f'./{dirname}/{c_filename}') else True
+    path = f"{common.is_parent_dir_required(is_parent_dir)}{dirname}/{common.clean_filename(filename, 'csv')}"
+    db_name = f"{common.is_parent_dir_required(parent=is_parent_dir)}{filename}-{datetime.date.today()}.db"
+    db_conn = sqlite3.connect(db_name)
+    db_cur = db_conn.cursor()
+
+    db_create_table = """
+    CREATE TABLE embeds(id,
+                        title,
+                        duration,
+                        date,
+                        categories,
+                        rating,
+                        thumbnail,
+                        embed_code,
+                        web_link,
+                        wp_slug)
+    """
+    db_cur.execute(db_create_table)
+
+    total_entries = 0
+    with open(path, "r", encoding="utf-8") as dump_file:
+        for line in dump_file.readlines():
+            # I don't want to process or count the header of the csv file.
+            if total_entries == 0:
+                total_entries += 1
+                continue
+            else:
+                line_split = line.split(sep)
+                id_ = line_split[0]
+                title = line_split[1]
+                duration = line_split[2]
+                publish_date = line_split[3].split(" ")[0]
+                categories = line_split[4]
+                rating = line_split[5]
+                main_thumbnail = line_split[6]
+                embed_code = line_split[7]
+                website_link = line_split[8]
+
+                # Custom db fields
+
+                # As mentioned in other modules, slugs have to contain the
+                # content type
+                wp_slug = f"{website_link.split('/')[-2:][0]}-{partner}-video"
+
+                all_values = (
+                    id_,
+                    title,
+                    duration,
+                    publish_date,
+                    categories,
+                    rating,
+                    main_thumbnail,
+                    embed_code,
+                    website_link,
+                    wp_slug
+                )
+
+                db_cur.execute(
+                    "INSERT INTO embeds values(?,?,?,?,?,?,?,?,?, ?)",
+                    all_values,
+                )
+                db_conn.commit()
+                total_entries += 1
+
+    db_conn.close()
+    return f"Inserted a total of {total_entries} video entries into {db_name}"
+
+
+if __name__ == '__main__':
+    arg_parser = argparse.ArgumentParser(description='PartnerEight integration - CLI Interface')
+
+    arg_parser.add_argument('-sort', type=str,
+                            help="""Sorting criteria from possible values:
+                                  1. popularity
+                                  2. rating
+                                  3. duration
+                                  4. post_date
+                                  5. id (default if no criteria is provided)""")
+
+    arg_parser.add_argument('-days', type=int,
+                            help='Provide the time period in days')
+
+    arg_parser.add_argument('-limit', type=int, help='amount of urls to process')
+
+    cli_args = arg_parser.parse_args()
+
+    # Build the dump URL
+    main_url = construct_feed_beta_dump_url(
+        PartnerOne_BASE_URL, cli_args.sort, cli_args.days, url_limit=cli_args.limit)
+
+    # Get the dump file and write it into a .csv file
+    common.write_to_file(
+        'feed_beta-dump',
+        'tmp',
+        'csv',
+        common.access_url_bs4(main_url))
+
+    # Parse the temporary CSV dump file
+    result = feed_beta_dump_parse("feed_beta-dump", "tmp", 'feed_beta', "|")
+
+    # Clean the temp .csv file in temporary folder
+    clean_file_cache('tmp', 'csv')
+    print('Cleaned temporary folder...')
+
+    print(result)
---
+++ integrations/url_builder.py
@@ -0,0 +1,42 @@
+"""
+Help in the construction of API URLs that other modules need to gather information from a remote source.
+Contribute to better organization of the code by providing common elements that some APIs need in this package.
+"""
+
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class CSVColumns:
+    """
+    CSV Column fields that can be used together or in isolation depending on the kind of
+    result that you want to obtain.
+    """
+    ID_: str = 'id'
+    title: str = 'title'
+    description: str = 'description'
+    link: str = 'link'
+    duration: str = 'duration'
+    rating: str = "rating"
+    added_time: str = "post_date"
+    categories: str = "categories"
+    tags: str = "tags"
+    model: str = "models"
+    embed_code: str = "embed"
+    # Attaches to main to fetch thumbnail.
+    thumbnail_prefix: str = "screenshots_prefix"
+    main_thumbnail: str = (
+        "main_screenshot"  # Thumbnail ID obtained from removing the img extension
+    )
+    thumbnails: str = "screenshots"
+    video_thumbnail_url: str = "preview_url"
+
+
+@dataclass(frozen=True)
+class CSVSeparators:
+    """
+    Encoded separators for the URL params
+    """
+    pipe_sep: str = "%7C"
+    comma_sep: str = "%2C"
+    semicolon_sep: str = "%3B"
