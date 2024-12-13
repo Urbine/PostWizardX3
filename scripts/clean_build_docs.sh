412b542a8f266b638c8b28815bc8fb52e899fc19
Urbine <yohamg@programmer.net>
2024-12-13 20:20:37 +0700

    chore(scripts): cd into docs dir before sphinx build
    Prepend cd ~/GitHub/webmaster-seo-tools/docs to clean_build_docs.sh so the build runs from the docs directory; blank lines between steps.

--- scripts/clean_build_docs.sh
+++ scripts/clean_build_docs.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+# Go to ``docs`` to find the /source directory
+cd ~/GitHub/webmaster-seo-tools/docs
+
 # Fetch docs from project docstrings
 sphinx-apidoc -o ./source ..
+
 # Clean previous build files...
 make clean
+
 # Generate the new docs page...
 make html
